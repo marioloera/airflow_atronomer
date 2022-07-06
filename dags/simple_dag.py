5c72a67b24903d295efbfd9f6f31f55117c02330
Mario Loera <mario.loera@trustly.com>
2022-07-06 19:22:47 +0200

    simple_dag dev

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,4 +1,20 @@
-from airflow import DAG
 
-with DAG(dag_id="simple_dag") as dag:
-    None
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+with DAG(
+    dag_id="simple_dag", 
+    # start_date=datetime(2022, 7, 5),
+    start_date=days_ago(3),
+    schedule_interval="@daily",
+    catchup=True,
+    ) as dag:
+
+    task_1 = DummyOperator(task_id="task_1")
+
+    task_2 = DummyOperator(task_id="task_2")
+
+    task_1 >> task_2
